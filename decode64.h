64faa045b7a1bf92af8b1a6cc4792073a8ba6018
connorknoetze <connorknoetze@gmail.com>
2025-08-04 21:31:54 +1200

    Added ability to store keys encrypted with a master key

---
+++ decode64.h
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Base64 decoding table
+static const unsigned char base64_table[256] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62, 0, 0, 0, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0, 0, 0, 0, 0,
+    0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 0, 0, 0, 0, 0
+};
+
+void decode_base64(const char *input, unsigned char **output, size_t *output_len);
+
+void decode_base64(const char *input, unsigned char **output, size_t *output_len) {
+    size_t input_len = strlen(input);
+    size_t padding = 0;
+
+    // Count padding characters ('=')
+    if (input_len >= 2 && input[input_len - 1] == '=')
+        padding++;
+    if (input_len >= 2 && input[input_len - 2] == '=')
+        padding++;
+
+    // Calculate output length
+    *output_len = (input_len / 4) * 3 - padding;
+    *output = (unsigned char *)malloc(*output_len);
+    if (*output == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t j = 0;
+    for (size_t i = 0; i < input_len; i += 4) {
+        unsigned int sextet_a = input[i] == '=' ? 0 & i++ : base64_table[(unsigned char)input[i]];
+        unsigned int sextet_b = input[i + 1] == '=' ? 0 & i++ : base64_table[(unsigned char)input[i + 1]];
+        unsigned int sextet_c = input[i + 2] == '=' ? 0 & i++ : base64_table[(unsigned char)input[i + 2]];
+        unsigned int sextet_d = input[i + 3] == '=' ? 0 & i++ : base64_table[(unsigned char)input[i + 3]];
+
+        unsigned int triple = (sextet_a << 18) | (sextet_b << 12) | (sextet_c << 6) | sextet_d;
+
+        if (j < *output_len) (*output)[j++] = (triple >> 16) & 0xFF;
+        if (j < *output_len) (*output)[j++] = (triple >> 8) & 0xFF;
+        if (j < *output_len) (*output)[j++] = triple & 0xFF;
+    }
+}
